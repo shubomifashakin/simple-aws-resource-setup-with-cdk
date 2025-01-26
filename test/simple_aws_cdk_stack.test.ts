02bb742ae48be07c4cd3e0f38c8fed77cdeed30b
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-01-26 18:18:21 +0100

    Initial commit

---
+++ test/simple_aws_cdk_stack.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SimpleAwsCdkStack from '../lib/simple_aws_cdk_stack-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/simple_aws_cdk_stack-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SimpleAwsCdkStack.SimpleAwsCdkStackStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
